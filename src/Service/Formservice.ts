91ebc0bdf065fa1763cda5366af09bfb933a632d
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-02-02 22:39:50 +0530

    Fix service URLs in BlogService, FormService, InternationalService, and TourService to remove duplicate slashes

--- src/Service/Formservice.ts
+++ src/Service/Formservice.ts
@@ -2,7 +2,7 @@ import { clientCall } from "../Client/axiosCall"
 import { emailtype, LogindataType } from "../Interfaces/Interface";
 
 export const postData=async(formdata:LogindataType)=>{
-try{const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/postData","POST",formdata)
+try{const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/postData","POST",formdata)
 return response}
 catch(error){
     console.log(error);
@@ -10,12 +10,12 @@ catch(error){
 }
 }
 export const getMembership=async()=>{
-    const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/getMembership","GET")
+    const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/getMembership","GET")
     return response
 }
 export const checklogin=async(formdata:LogindataType)=>{
     try{
-    const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/checkLogin","POST",formdata)
+    const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/checkLogin","POST",formdata)
     return response
     }
     catch(error){
@@ -25,7 +25,7 @@ export const checklogin=async(formdata:LogindataType)=>{
 }
 export const emailExist=async(formdata:emailtype)=>{
     try{
-    const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/emailExist","POST",formdata)
+    const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/emailExist","POST",formdata)
     return response
     }
     catch(error){
@@ -35,7 +35,7 @@ export const emailExist=async(formdata:emailtype)=>{
 }
 export const updatepassword=async(formdata:LogindataType)=>{
     try{
-    const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/updatepassword","POST",formdata)
+    const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/updatepassword","POST",formdata)
     return response
     }
     catch(error){
@@ -45,7 +45,7 @@ export const updatepassword=async(formdata:LogindataType)=>{
 }
 export const bookingTour=async(data:any)=>{
     try{    
-    const response=await clientCall("https://mynodeapp-production.up.railway.app//myAdmin/bookingtour","POST",data)
+    const response=await clientCall("https://mynodeapp-production.up.railway.app/myAdmin/bookingtour","POST",data)
     return response
     }
     catch(error){
